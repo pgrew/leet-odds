d2a2b39347e72c2257a34fe2a4227c860bdced7f
pgrew <pgrew@protonmail.com>
2021-04-14 18:07:19 -0500

    make stack an int64

--- cardtable/player.go
+++ cardtable/player.go
@@ -1,6 +1,10 @@
 package cardtable
 
 type Player struct {
-	hand  Hand
-	stack []Chip
+	isActing bool
+	isMucked bool
+	amountPotOwes int64
+	numberOfPotSplashes int64
+	stack int64 //todo chip
+  hand Hand
 }
